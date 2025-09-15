09873505514a828d324d5f4119e39635dc1edabd
Janusae <moiworktandis@gmail.com>
2025-09-15 10:09:45 +0330

    feature : Complete Remove Duplicates exercise

---
+++ Day4/RemoveDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Reverse_String.Day4
+{
+    public class RemoveDuplicates
+    {
+        public static string Remove(string text)
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+            if (text.Length is 0)
+                return "";
+
+            var result = new StringBuilder();
+            var repeatedIteme = new HashSet<char>();
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (!repeatedIteme.Contains(text.ToLower()[i]))
+                {
+                    repeatedIteme.Add(text.ToLower()[i]);
+                    result.Append(text[i]);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Program
+﻿using Reverse_String.Day4;
+
+namespace Program
 {
     public class main
     {
         public static void Main()
         {
+            Console.WriteLine(RemoveDuplicates.Remove("Hello"));
         }
     }
 }
