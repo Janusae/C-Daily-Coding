41f61dfbbea003f7640fb062363f1dd73f8de60f
Janusae <moiworktandis@gmail.com>
2025-09-15 10:09:44 +0330

    Add second program

---
+++ Day2/Palindrome.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+namespace Reverse_String.Day2
+{
+    public class Palindrome
+    {
+        public static bool Palindromer(string text)
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
+            if (text is "")
+                return false;
+
+            var reversed = new StringBuilder(text.Length);
+            for(int i = text.Length -1; i>=0; i--)
+            {
+                reversed.Append(text[i]);
+            }
+            return string.Equals(reversed.ToString(), text, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿using ReverseString;
+﻿using Reverse_String.Day2;
 namespace Program
 {
     public class main
     {
         public static void Main()
         {
+            Console.WriteLine(Palindrome.Palindromer("David is here"));
         }
     }
 }
