c9facfac90979ff71dfc4b8a265be1ea955e2098
Janusae <moiworktandis@gmail.com>
2025-09-15 10:09:45 +0330

    feature : compelete Vowels exercise

---
+++ Day3/CountVowels.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse_String.Day3
+{
+    public class CountVowels
+    {
+        public static int Count(string text)
+        {
+            if(text is null)
+                throw new ArgumentNullException("text");
+
+            if (text.Length == 0)
+                return 0;
+
+            HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u'};
+            var count = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (vowels.Contains(char.ToLower(text[i]))) count++;
+            }
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
-﻿using Reverse_String.Day2;
-namespace Program
+﻿namespace Program
 {
     public class main
     {
         public static void Main()
         {
-            Console.WriteLine(Palindrome.Palindromer("David is here"));
         }
     }
 }
